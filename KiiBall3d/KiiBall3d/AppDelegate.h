1f298164f89236491976aa2f171c0774e850fbc2
Riza <riza.alaudin@kii.com>
2014-07-16 14:41:07 +0900

    initial

---
+++ KiiBall3d/KiiBall3d/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KiiBall3d
+//
+//  Created by Syah Riza on 7/16/14.
+//  Copyright (c) 2014 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KiiBall3d/KiiBall3d/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  KiiBall3d
+//
+
+//  Copyright (c) 2014 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
